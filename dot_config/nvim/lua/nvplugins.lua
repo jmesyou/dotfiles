c2f3f8634fe0248a98c7352668cc202d5d5aa218
James You <jyou@ibm.com>
2023-06-01 13:54:29 -0400

    add nvim config

---
+++ dot_config/nvim/lua/nvplugins.lua
@@ -0,0 +1,28 @@
+local Plugins = {}
+
+function Plugins.get()
+  return {
+    {
+      "folke/tokyonight.nvim",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+      config = function()
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]])
+      end,
+    },
+    {
+      "nvim-tree/nvim-tree.lua",
+      version = "*",
+      dependencies = {
+        "nvim-tree/nvim-web-devicons",
+      },
+      config = function()
+        require("nvim-tree").setup {}
+      end, 
+    }
+  }
+end
+
+return Plugins
+
