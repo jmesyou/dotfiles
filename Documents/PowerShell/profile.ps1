4d37cd73ec3f5429cca89944dbe7af8598e7b930
James You <james.you@protonmail.com>
2024-05-22 18:31:17 -0400

    add eza alias to pwsh

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,3 +1,8 @@
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+  Set-Alias -Name ls -Value eza
+}
+
+
 # Starship
 if (Get-Command starship -ErrorAction SilentlyContinue) {
   Invoke-Expression (&starship init powershell)
