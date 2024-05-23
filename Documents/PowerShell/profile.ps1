508b085f70e9836387677e6bb2f51fcdce6c7433
James You <james.you@protonmail.com>
2024-05-23 14:35:00 -0400

    improve powershell profile

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,9 +1,17 @@
-if (Get-Command starship -ErrorAction SilentlyContinue) {
+function Check-Command($Name) {
+  return [bool](Get-Command -Name $Name -ErrorAction SilentlyContinue)
+}
+
+if (Check-Command -Name eza) {
   Set-Alias -Name ls -Value eza
 }
 
-
 # Starship
-if (Get-Command starship -ErrorAction SilentlyContinue) {
+if (Check-Command -Name starship) {
   Invoke-Expression (&starship init powershell)
+}
+
+# direnv
+if (Check-Command -Name direnv) {
+  Invoke-Expression "$(direnv hook pwsh)"
 }
