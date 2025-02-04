e9656109cf82f3fd9cf653f7f7efe15e2224ab82
James You <james.you@protonmail.com>
2025-02-04 17:07:45 -0500

    update powershell configuration

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -2,17 +2,21 @@ function Check-Command($Name) {
   return [bool](Get-Command -Name $Name -ErrorAction SilentlyContinue)
 }
 
+function Module-Exists($Name) {
+  return [bool](Get-Module -ListAvailable -Name $Name)
+}
+
 if (Check-Command -Name eza) {
   Set-Alias -Name ls -Value eza
 }
 
-# Starship
-if (Check-Command -Name starship) {
+if (Check-Command -Name oh-my-posh) {
+  $Theme = "tokyo"
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/$Theme.omp.json" | Invoke-Expression
+} elseif (Module-Exists PowerLine) {
+  Import-Module PowerLine
+} elseif (Check-Command starship) {
   Invoke-Expression (&starship init powershell)
 }
 
-# direnv
-if (Check-Command -Name direnv) {
-  Invoke-Expression "$(direnv hook pwsh)"
-}
 
